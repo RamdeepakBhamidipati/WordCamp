89b6c3ae73cabc0da3d9ab5d9ae84f4f1a5e3f76
RamdeepakBhamidipati <ramdeepak.bhamidipati@tekyantra.com>
2024-11-09 08:20:30 +0530

    word camp changes

---
+++ src/main/java/com/example/todoapp/Models/CouponInput.java
@@ -0,0 +1,27 @@
+package com.example.todoapp.Models;
+
+public class CouponInput {
+
+    private String email;
+
+    private String coupon;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(String coupon) {
+        this.coupon = coupon;
+    }
+
+    public CouponInput() {
+    }
+}
